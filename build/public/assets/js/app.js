7a606a38baa042072acc7a793c9b298489e79c64
infoozle.com <infoozle.com@gmail.com>
2023-02-17 18:21:16 -0400

    upadte files

--- build/public/assets/js/app.js
+++ build/public/assets/js/app.js
@@ -1,19 +1,20 @@
-"use strict";
 console.log("ok");
 let getAllLinks = document.querySelectorAll("#nakama_link_copy");
 getAllLinks.forEach((createButtonCopy, index) => {
-    createButtonCopy.addEventListener("click", () => {
-        let assingStatusCopy = document.querySelectorAll("#nakama_link_status");
-        let createTmpElement = document.createElement("textarea");
-        createTmpElement.value = createButtonCopy.textContent.trim();
-        document.body.appendChild(createTmpElement);
-        createTmpElement.select();
-        if (document.execCommand("copy")) {
-            createTmpElement.remove();
-            assingStatusCopy[index].innerHTML = `<p class="bg-primary p-2 rounded text-white">Copy Success Nakama</p>`;
-            setTimeout(() => {
-                assingStatusCopy[index].innerHTML = "";
-            }, 1800);
-        }
-    });
+  createButtonCopy.addEventListener("click", () => {
+    let assingStatusCopy = document.querySelectorAll("#nakama_link_status");
+    let createTmpElement = document.createElement("textarea");
+    createTmpElement.value = createButtonCopy.textContent.trim();
+    document.body.appendChild(createTmpElement);
+    createTmpElement.select();
+    if (document.execCommand("copy")) {
+      createTmpElement.remove();
+      assingStatusCopy[
+        index
+      ].innerHTML = `<p class="bg-primary p-2 rounded text-white">Copy Success Nakama</p>`;
+      setTimeout(() => {
+        assingStatusCopy[index].innerHTML = "";
+      }, 1800);
+    }
+  });
 });
